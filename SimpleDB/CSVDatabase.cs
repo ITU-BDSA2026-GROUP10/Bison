1866824bb0bb5c8f3a65e7d8f353a33e732510bc
Sofie Helt <lesh@itu.dk>
2026-09-04 14:50:54 +0200

    Fix SimpleDB: fixed filed names

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,26 +1,27 @@
-﻿namespace simpleDB;
+﻿namespace SimpleDB;
 
 using CsvHelper;
 using System.ComponentModel.Design;
 using System.Globalization;
 
-sealed class CSVDatabase<T> : IDatabaseRepository<T> 
+sealed class CSVDatabase<T> /*: IDatabaseRepository<T> */
 {
-    public IEnumerable<T> Read (int? limit = null) {
-        IEnumerable <Cheep> objects;
+     
+    public /*IEnumerable<T> */ void Read(int? limit = null) {
+        IEnumerable <T> objects;
         var reader = new StreamReader("bison_observe_cli_db.csv");
                 var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
                 
-                var records = csv.GetRecords<Cheep>();
+                var records = csv.GetRecords<T>();
                 Console.WriteLine(records);
 
                 
                 foreach (var r in records)
                 {
-                    DateTime time = DateTimeOffset.FromUnixTimeSeconds(r.Timestamp).DateTime;
-                    Console.WriteLine(r.Author + " @ " + time + ": " + r.Observation.Trim('\"'));
+                   //$ion.Trim('\"'));
                     //Console.WriteLine($"{r.Author}, {r.Observation}, {r.Timestamp}");
                 }
+
     }
  
     public void Store(T record) {
