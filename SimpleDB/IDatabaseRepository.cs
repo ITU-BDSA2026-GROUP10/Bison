ab8a869f83f3018f68ffcee92da8058dfda119bd
Signe <sibs@itu.dk>
2026-09-05 11:09:21 +0200

    Moved store logic from program.cs to CSVDatabase.cs

--- SimpleDB/IDatabaseRepository.cs
+++ SimpleDB/IDatabaseRepository.cs
@@ -1,11 +1,6 @@
 namespace SimpleDB;
 interface IDatabaseRepository<T>
 {
-<<<<<<< HEAD
     public IEnumerable<T> Read(int? limit = null);
     public void Store(T record);
-=======
-public IEnumerable<T> Read(int? limit = null);
-public void Store(T record);
->>>>>>> main
 }
