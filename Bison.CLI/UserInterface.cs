4a279a30ebbf46b86992dbb35be1d6258fb351ab
Sofie Helt <lesh@itu.dk>
2026-09-06 20:52:09 +0200

    Feature Program,UserInterfacce,Comment,Observation: Made subclasses for Cheep so that it can be distinguished between a observation or comment. Also made two seperate csv files, one for comments and one for observations

--- Bison.CLI/UserInterface.cs
+++ Bison.CLI/UserInterface.cs
@@ -5,7 +5,15 @@ public static class UserInterface
         foreach (var r in obs)
         {
             DateTime time = DateTimeOffset.FromUnixTimeSeconds(r.Timestamp).DateTime;
-            Console.WriteLine(r.Author + " @ " + time + ": " + r.Observation.Trim('\"'));
+            if (typeof(Observation).IsInstanceOfType(r))
+            {
+                Observation observation = (Observation)r;
+                Console.WriteLine(r.Author + " @ " + time + ": " + observation.observation.Trim('\"'));
+            } else if (typeof(Comment).IsInstanceOfType(r))
+            {
+                Comment comment = (Comment)r;
+                Console.WriteLine(r.Author + " @ " + time + ": " + comment.observationId);
+            }
         }
     }
 }
