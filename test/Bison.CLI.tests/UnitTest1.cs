cc434db2239343a2f0096511bc8f74bdd1ecb627
Raamin <rraz@itu.dk>
2026-09-12 16:09:35 +0200

    Build Test: Created xunit dotnet tests and added references to projects
    Co-authored-by: Louise lobi@itu.dk

---
+++ test/Bison.CLI.tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Bison.CLI.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/SimpleDB.tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SimpleDB.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
