ad922815504b7c8580fbeecdf1881b62eea6e435
Sofie Helt <lesh@itu.dk>
2026-09-14 10:35:14 +0200

    Feature : Added a minimal web API Co-authored-by: Tekla tesv@itu.dk Co-authored-by: Signe sibs@itu.dk Co-authored-by: Raamin rraz@itu.dk Co-authored-by: Louise lobi@itu.dk

--- src/Bison.CLI/Bison.CSVDBService/Program.cs
+++ src/Bison.CLI/Bison.CSVDBService/Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/observations", () => new Observation("Peter", "I saw the heron again!", 1684229348));
 app.Run();
+public record Observation(string Author, string Message, long Timestamp);
