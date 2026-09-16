ff3e47b4fd3776beef315e48294fbd00267328d0
Sofie Helt <lesh@itu.dk>
2026-09-16 10:42:12 +0200

    Feature : Changed parameters of store comment

--- src/SimpleDB/IDatabaseRepository.cs
+++ src/SimpleDB/IDatabaseRepository.cs
@@ -6,4 +6,6 @@ interface IDatabaseRepository<T>
     public IEnumerable<T> Read(string path, int? limit = null);
 
     public void Store(T record, string path);
+
+    public void StoreComment(T record, string path, string observePath, long ObservationID);
 }
