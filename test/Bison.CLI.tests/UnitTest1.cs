e863a89a92cd69ac8f3b2fddf0a8ef8d076a9a11
Signe <sibs@itu.dk>
2026-09-20 13:37:34 +0200

    Test: Tried modifying a previous test

--- test/Bison.CLI.tests/UnitTest1.cs
+++ test/Bison.CLI.tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 namespace Bison.CLI.tests;
+using SimpleDB;
 
 public class UnitTest1
 {
@@ -14,4 +15,15 @@ public class UnitTest1
         //Assert
         Assert.Equal("13/09/2026 17.34.06", dateTime.ToString());
     }
+
+    /*[Fact]
+    public void CommentToNonExistingObservationReturnsFalse()
+    {
+        //Arrange
+        Comment comment = new Comment(Environment.UserName, "This is a test comment", DateTimeOffset.Now.ToUnixTimeSeconds() + 7200, 10);
+        //Act
+
+        //Assert
+        Assert.False(UserInterface.ObservationExists(2, comment.ObservationId));   
+    }*/
 }
