7a28ab2d5d595ac018021adabd90ba0753be7eb1
Signe <sibs@itu.dk>
2026-09-20 11:32:10 +0200

    Moved the test that tests the timestamp method to Bison.CLI tests

--- test/Bison.CLI.tests/UnitTest1.cs
+++ test/Bison.CLI.tests/UnitTest1.cs
@@ -3,8 +3,15 @@ namespace Bison.CLI.tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void UnixTimeConvertsCorrectlyToUserReadableTime()
     {
+        //Arrange
+        long unixTime = 1789313646+7200;
 
+        //Act
+        DateTime dateTime = UserInterface.GetDateTime(unixTime);
+        
+        //Assert
+        Assert.Equal("13/09/2026 17.34.06", dateTime.ToString());
     }
 }
