39f686eff5ddbd7b952351cb4a5f547c99461205
Raamin <rraz@itu.dk>
2026-09-25 18:14:49 +0200

    Feature: end-to-end tests: Created end to end test, to ensure our program exits correctly
    Co-authored-by: Sofie <lesh@itu.dk>

--- test/Bison.CLI.tests/EndToEndTest.cs
+++ test/Bison.CLI.tests/EndToEndTest.cs
@@ -1,17 +1,50 @@
 global using Xunit;
 
+
 using System.CommandLine;
+using Bison.CLI;
 using System.Net;
 using System.Net.Http;
 
+
 public class EndtoEndTest
 {
-    [Fact] 
-    public async Task ProgramExistsCorrectlyAfterCommand ()
-    {
-        var rootCommand = new RootCommand();
+   [Fact]
+   public async Task ProgramExistsCorrectlyAfterObserveCommand ()
+   {
+       //Arrange
+       var args = new string[] {"observe", "Heron DR Byen", "DR Byen"};
 
-        var exitCode = await rootCommand.InvokeAsync("observe Heron DR Byen");
-        Assert.Equal(0, exitCode);
-    }
+
+       //Act
+       int result = await Program.Main(args);
+
+
+       Task <int> exitcode = new Task<int>(() => 0);
+       exitcode.Start();
+       await exitcode;
+      
+       //Assert
+       Assert.Equal(0, result);
+   }
+
+
+[Fact]
+public async Task ProgramExistsCorrectlyAfterReadCommand ()
+   {
+       //Arrange
+       var args = new string[] {"read"};
+
+
+       //Act
+       int result = await Program.Main(args);
+
+
+       Task <int> exitcode = new Task<int>(() => 0);
+       exitcode.Start();
+       await exitcode;
+      
+       //Assert
+       Assert.Equal(0, result);
+   }
 }
